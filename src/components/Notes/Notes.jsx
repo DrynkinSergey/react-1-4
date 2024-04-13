63b93f6b60852e5dbb45a88adb405fabf489eab3
Sergey <sergsmile29@gmail.com>
2024-04-13 10:40:08 +0300

    useState

--- src/components/Notes/Notes.jsx
+++ src/components/Notes/Notes.jsx
@@ -1,22 +1,39 @@
+import { useState } from 'react'
+
 export const Notes = () => {
-	const showSum = (a, b) => {
-		console.log(+a + +b)
-		console.log('First number: ' + a)
-		console.log('Second number: ' + b)
-	}
-	const sayHi = name => {
-		console.log('Hello ' + name)
+	const [isOpen, setIsOpen] = useState(false)
+	const [name, setName] = useState('Alex')
+	const [city, setCity] = useState('Lviv')
+	const [clicks, setClicks] = useState(0)
+	// const isOpen = true
+
+	const handleOpen = () => {
+		console.log(isOpen)
+		setIsOpen(!isOpen)
+		console.log(isOpen)
 	}
 
+	const handleClick = () => {
+		setName(prompt('Enter new name'))
+	}
+	const handleChangeCity = () => {
+		setCity(prompt('Enter new city'))
+	}
 	return (
 		<div>
 			<h2>Notes</h2>
-			<button onClick={() => alert('Hello')}>Hello</button>
-			<button onClick={() => showSum(prompt('Enter second number'), prompt('Enter first number'))}>Show sum</button>
-			<button onClick={() => showSum(1, 4)}>Show sum 2</button>
-			<button onClick={() => showSum(10, '14')}>Show sum 3</button>
-
-			<button onClick={() => sayHi('Sasha')}>Show sum 3</button>
+			<h2>Hello, {name}</h2>
+			<h3>Your city {city}</h3>
+			<button onClick={handleOpen}> {isOpen ? 'Close' : 'Open'} </button>
+			{isOpen && (
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad impedit odio, minus illum dolores consectetur eius
+					dolorem fuga quas voluptates culpa libero velit ratione, veniam fugit cum accusamus quam dolore.
+				</p>
+			)}
+			<button onClick={handleClick}>Change name</button>
+			<button onClick={handleChangeCity}>Change city</button>
+			<button onClick={() => setClicks(prev => prev + 1)}>Clicks: {clicks}</button>
 		</div>
 	)
 }

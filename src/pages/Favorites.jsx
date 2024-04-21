d26f2507c8e3cf3500fdbc1680abedd6970c3bc6
Sergey <sergsmile29@gmail.com>
2024-04-21 13:34:10 +0300

    update

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,7 +1,10 @@
 import { useEffect, useState } from 'react'
 import { ImageList } from '../components/pexels/ImageList'
+import { toast } from 'react-toastify'
 
 const Favorites = () => {
+	const [value, setValue] = useState('')
+	const [access, setAccess] = useState(false)
 	const [favorites, setFavorites] = useState(() => {
 		const savedFav = JSON.parse(window.localStorage.getItem('favorites'))
 		if (savedFav?.length) {
@@ -15,6 +18,24 @@ const Favorites = () => {
 	const remoteFromFav = item => {
 		setFavorites(prev => prev.filter(el => el.id !== item.id))
 	}
+
+	const handleGetAccess = () => {
+		const access = import.meta.env.VITE_ACCESS === value
+		if (access) {
+			return setAccess(true)
+		}
+		toast.error('Wrong access')
+	}
+	if (!access) {
+		return (
+			<div>
+				<h1>Please sign in</h1>
+				<input type='text' value={value} onChange={e => setValue(e.target.value)} />
+				<button onClick={handleGetAccess}>Sign in</button>
+			</div>
+		)
+	}
+
 	return (
 		<div>
 			<h1>Favorites images</h1>
